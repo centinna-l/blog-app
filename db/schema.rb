3af13e7847bf3399577b7f888ec4293880954ae8
centinna-l <jerryjoy99@gmail.com>
2022-01-19 01:00:19 +0530

    Changes in schema and CRUD routes done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_13_145440) do
+ActiveRecord::Schema.define(version: 2022_01_18_104737) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
